b4227b0092fd6c676973243a65072f8ecd3c0959
Coolbrother <coolbrother@sourcegod.com>
2021-11-21 20:38:47 +0100

    Added file: (miditools.py, SampleObj class, change_freq and change_note functions)

--- src/stepyseq.py
+++ src/stepyseq.py
@@ -11,7 +11,20 @@ import time
 import numpy as np
 import pyaudio
 from collections import deque
+import miditools
 _pa = pyaudio.PyAudio()
+
+class SampleObj(object):
+    def __init__(self, freq=0, _len=0):
+        self.freq = freq
+        self.note =0
+        self.data_len = _len
+        self.raw_data = None
+  
+    #-------------------------------------------
+
+#========================================
+
 
 class BaseDriver(object):
     def __init__(self):
@@ -86,17 +99,17 @@ class PortDriver(BaseDriver):
 
 class WaveGenerator(object):
     """ generate waveform """
-    def __init__(self, rate=44100, channels=1):
+    def __init__(self, rate=44100, channels=1, _len=1):
         self._rate = rate
         self._channels = channels
+        self._len = _len
 
     #-------------------------------------------
 
-    def gen_samples(self, freq=440, length=1):
-        # freq = 440
-        # rate = 48000
-        # length =1 # length samples
-        nb_samples = int(length * self._rate)
+    def gen_samples(self, freq=440, _len=0):
+        if _len == 0:
+            _len = self._len
+        nb_samples = int(_len * self._rate)
         # create an array
         x = np.arange(nb_samples)
         # the math function, is also the final sample
@@ -121,42 +134,94 @@ class Pattern(object):
             self._bpm = 120
         self._tempo = float(self._nbClockMsec / self._bpm) # in millisec
         self._nbSamples = int( (self._tempo * self._rate / 1000) * (4 / self._nbNotes) ) # in samples
-        self._noteLst = [] # [0] * self._nbNotes
+        self._sampLst = [] # [0] * self._nbNotes
+        self._paramLst = []
         self._audioData = None
     
     #-------------------------------------------
 
-    def set_bpm(self, bpm):
-        if bpm >= self._minBpm and bpm <= self._maxBpm:
-            self._bpm = bpm
-        self._tempo = float(self._nbClockMsec / self._bpm) # in millisec
-        self._nbSamples = int( (self._tempo * self._rate / 1000) * (4 / self._nbNotes) ) # in samples
-        self.gen_audio()
- 
-    #-------------------------------------------
-    
+   
     def get_bpm(self):
         return self._bpm
 
     #-------------------------------------------
 
+    def set_bpm(self, bpm):
+        if bpm >= self._minBpm and bpm <= self._maxBpm:
+            self._bpm = bpm
+            self._tempo = float(self._nbClockMsec / self._bpm) # in millisec
+            self._nbSamples = int( (self._tempo * self._rate / 1000) * (4 / self._nbNotes) ) # in samples
+            # self.gen_audio()
+     
+    #-------------------------------------------
+ 
+    def get_freq(self, index):
+        try:
+            return self._sampLst[index].freq
+        except IndexError:
+            return 0
     
-    def add_notes(self, *notes):
-        """ adding notes to the list """
-        self._noteLst.extend(notes)
+    #-------------------------------------------
+
+    def set_freq(self, index, freq):
+        if freq >= 0 and freq <= 20000:
+            try:
+                self._sampLst[index].freq = freq
+            except IndexError:
+                pass
+    
+    #-------------------------------------------
+
+    def get_note(self, index):
+        try:
+            return self._sampLst[index].note
+        except IndexError:
+            return 0
+    
+    #-------------------------------------------
+
+    def set_note(self, index, note):
+        if note >= 0 and note <= 127:
+            try:
+                self._sampLst[index].note = note
+            except IndexError:
+                pass
+    
+    #-------------------------------------------
+  
+    def set_sample(self, index, samp):
+        try:
+            self._sampLst[index] = samp
+            # self.gen_audio()
+        except IndexError:
+            pass
+ 
+    #-------------------------------------------
+
+    def get_sample(self, index):
+        try:
+            return self._sampLst[index]
+        except IndexError:
+            return 
+ 
+    #-------------------------------------------
+      
+    def set_sampleList(self, samp_lst):
+        """ init sample list """
+        self._sampLst = samp_lst
 
     #-------------------------------------------
 
-    def get_notes(self):
-        return self._noteLst
+    def get_sampleList(self):
+        return self._sampLst
 
     #-------------------------------------------
 
     def gen_audio(self):
         audioData = []
-        sampLst = self.get_notes()
-        for samp in sampLst:
-            audioData.extend(samp[0:self._nbSamples])
+        samp_lst = self.get_sampleList()
+        for samp in samp_lst:
+            audioData.extend(samp.raw_data[0:self._nbSamples])
         
         audioData = np.float32(audioData).tobytes()
         self._audioData = audioData
@@ -177,7 +242,9 @@ class AudioManager(BaseDriver):
     def __init__(self):
         super().__init__()
         self._audioDriver = PortDriver()
-        self._waveGen = WaveGenerator(self._rate, 1)
+        _len =2 # in sec
+        self._waveGen = WaveGenerator(self._rate, self._channels, _len)
+        self._midTools = miditools
         self._audioData = None
         self._dataLen =0
         self._deqData = deque()
@@ -208,7 +275,7 @@ class AudioManager(BaseDriver):
                 self._waveGen.gen_samples(560, 1),
                 self._waveGen.gen_samples(700, 1),
                 ]
-        pat.add_notes(*sampLst)
+        pat.set_sampleList(sampLst)
         
         for samp in sampLst:
             smp.extend(samp[0:24000])
@@ -245,12 +312,23 @@ class AudioManager(BaseDriver):
         audioData = []
         pat = Pattern(bpm)
         samp_len =2 # in secs
+        samp_lst = []
+        midnote_lst = [72, 76, 79, 84]    
+        for note in midnote_lst:
+            freq = self._midTools.mid2freq(note) # C5
+            samp_obj = SampleObj(freq=freq, _len=samp_len)
+            samp_obj.note = note
+            samp_obj.raw_data = self._waveGen.gen_samples(samp_obj.freq, samp_obj.data_len)
+            samp_lst.append(samp_obj)
+
+        """
         sampLst = [self._waveGen.gen_samples(880, samp_len),
                 self._waveGen.gen_samples(440, samp_len),
                 self._waveGen.gen_samples(560, samp_len),
                 self._waveGen.gen_samples(700, samp_len),
                 ]
-        pat.add_notes(*sampLst)
+        """
+        pat.set_sampleList(samp_lst)
 
         audioData = pat.gen_audio()
         
@@ -277,12 +355,45 @@ class AudioManager(BaseDriver):
             bpm = self._pat.get_bpm() + bpm
 
         self._pat.set_bpm(bpm)
+        self._pat.gen_audio()
         self.init_params()
-        self.print_info("bpm")
+        msg = f"Bpm: {bpm}"
+        self.print_info(msg)
 
     #-------------------------------------------
 
-    
+    def change_freq(self, index, freq, inc=0):
+        assert self._pat
+        if inc == 1: # is incremental
+            freq = self._pat.get_freq(index) + freq
+            pass
+
+        samp_len =2 # in sec
+        samp_obj = SampleObj(freq, samp_len)
+        samp_obj.raw_data = self._waveGen.gen_samples(freq, samp_len)
+        self._pat.set_sample(index, samp_obj)
+        self._pat.gen_audio()
+        self.init_params()
+        freq = self._pat.get_freq(index)
+        msg = f"Freq: {freq}"
+        self.print_info(msg)
+
+    #-------------------------------------------
+
+    def change_note(self, index, note, inc=0):
+        assert self._pat
+        if inc == 1: # is incremental
+            note = self._pat.get_note(index) + note
+
+        self._pat.set_note(index, note)
+        freq = self._midTools.mid2freq(note)
+        self.change_freq(index, freq, inc)
+        msg = f"Note: {note}"
+        self.print_info(msg)
+
+    #-------------------------------------------
+
+     
     def init_pos(self):
         if not self._pat: return
         self._index =0
@@ -332,11 +443,7 @@ class AudioManager(BaseDriver):
  
     def print_info(self, info):
         assert self._pat
-        if info == "bpm":
-            val = self._pat.get_bpm()
-            print(f"Bpm:  {val}")
-        else:
-            print(info)
+        print(info)
 
     #-------------------------------------------
 
@@ -351,8 +458,7 @@ def main():
     valStr = ""
     savStr = ""
     while 1:
-        key = ""
-        param = ""
+        key = param1 = param2 = ""
         valStr = input("-> ")
         if valStr == '': valStr = savStr
         else: savStr = valStr
@@ -362,7 +468,8 @@ def main():
             lst = valStr.split()
             lenLst = len(lst)
             if lenLst >0: key = lst[0]
-            if lenLst >1: param = lst[1]
+            if lenLst >1: param1 = lst[1]
+            if lenLst >2: param2 = lst[2]
 
         if key == 'q':
             print("Bye Bye!!!")
@@ -382,17 +489,23 @@ def main():
             audi_man.change_bpm(-10, inc=1)
         elif key == 'ff':
             audi_man.change_bpm(120, inc=0) # not incremental
+
         elif key == 'bpm':
-            if param:
-                audi_man.change_bpm(int(param), inc=0) # not incremental
+            if param1:
+                audi_man.change_bpm(float(param1), inc=0) # not incremental
             else:
                 audi_man.change_bpm(120, 0)
+        elif key == "freq":
+            if not param1: param1 =0
+            if not param2: param2 =440
+            audi_man.change_freq(int(param1), float(param2), inc=0) # not incremental
 
+        elif key == "note":
+            if not param1: param1 =0
+            if not param2: param2 =69 # A4
+            audi_man.change_note(int(param1), int(param2), inc=0) # not incremental
 
 #-------------------------------------------
-        elif key == 'ff':
-            audi_man.change_bpm(120, inc=0) # not incremental
-            audi_man.print_info("bpm")
 
 if __name__ == "__main__":
     main()
